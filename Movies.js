7e7a887194d4a3e36b08f01885d4ded9eeca9f62
Hasan-1000 <Hasan.alfaili@ucdenver.edu>
2026-04-15 03:42:33 -0600

    new movies

--- Movies.js
+++ Movies.js
@@ -3,9 +3,21 @@ var Schema = mongoose.Schema;
 
 mongoose.connect(process.env.DB);
 
+// Actor sub-schema
+var ActorSchema = new Schema({
+  actorName: { type: String, required: true },
+  characterName: { type: String, required: true }
+});
+
 // Movie schema
 var MovieSchema = new Schema({
+  title: { type: String, required: true },
+  releaseDate: { type: Number },
+  genre: { type: String },
 
+  imageUrl: { type: String },   
+
+  actors: [ActorSchema]         
 });
 
 // return the model
